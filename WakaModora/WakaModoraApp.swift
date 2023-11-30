83d98750bd33d2791c094c2744672bba0cb27d7a
SeikaHirori <SeikaHirori@outlook.com>
2023-11-30 11:48:21 -0800

    Initial Commit

---
+++ WakaModora/WakaModoraApp.swift
@@ -0,0 +1,17 @@
+//
+//  WakaModoraApp.swift
+//  WakaModora
+//
+//  Created by Seika Hirori on 11/30/23.
+//
+
+import SwiftUI
+
+@main
+struct WakaModoraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
